8d5dbeae61f38166c92664d784039d0b42e360b2
Anthony Kirwan <amkirwan@gmail.com>
2011-02-25 11:53:39 -0500

    added shared/erro_messages partial for forms

--- spec/views/admin/users/_form.html.haml_spec.rb
+++ spec/views/admin/users/_form.html.haml_spec.rb
@@ -8,48 +8,58 @@ describe "admin/users/_form.html.haml" do
     end
   end
   
+  def do_render(label)
+    render "form", :user => user, :button_label => label
+  end
+  
+  let(:user) { stub_model(User)}
+  let(:current_user) { stub_model(User, :role? => true) }
+  
+  before(:each) do
+    assign(:user, user)  
+    assign(:current_user, current_user)
+  end
+  
+  it "should render a shared error partial" do
+    do_render("label")   
+    view.should render_template(:partial => "shared/error_messages", :locals => { :target => user })
+  end
+  
   context "when the user is a new record" do
-    let(:user) { stub_model(User).as_new_record }
-    let(:current_user) { stub_model(User, :role? => true) }
     before(:each) do 
-      assign(:user, user)  
-      assign(:current_user, current_user)
-    end   
-    
-    def do_render
-      render "form", :user => user, :button_label => "Create User"
-    end  
+      user.as_new_record
+    end    
     
     it "should render a form to create a new user" do
-      do_render 
+      do_render("Create User") 
       form(:action => admin_users_path) do |form|
         form.should have_selector("input", :type => "submit", :value => "Create User")   
       end
     end 
     
     it "should show a field to enter the partners username" do
-      do_render
+      do_render("Create User")
       form(:action => admin_users_path) do |form|
         form.should have_selector("input", :type => "text", :name => "user[puid]")
       end
     end                                                                             
 
     it "should show a field to enter the username" do
-      do_render
+      do_render("Create User")
       form(:action => admin_users_path) do |form|
         form.should have_selector("input", :type => "text", :name => "user[username]")
       end
     end                                          
 
     it "should show a field to enter an email address" do
-      do_render 
+      do_render("Create User") 
       form(:action => admin_users_path) do |form|
         form.should have_selector("input", :type => "email", :name => "user[email]")
       end
     end
 
     it "should show radio button to set user role" do
-      do_render
+      do_render("Create User")
       form(:action => admin_users_path) do |form|
         form.should have_selector("input", :type => "radio", :name => "user[role]", :value => "editor")
         form.should have_selector("input", :type => "radio", :name => "user[role]", :value => "admin")   
@@ -57,7 +67,7 @@ describe "admin/users/_form.html.haml" do
     end  
     
     it "should show a cancel link" do
-      do_render
+      do_render("Create User")
       form(:action => admin_users_path) do |form|
         form.should have_selector("a.cancel", :href => admin_users_path)
       end
@@ -65,26 +75,20 @@ describe "admin/users/_form.html.haml" do
   end 
   
   context "when editing an existing user record" do
-    let(:user) { stub_model(User, :new_record? => false)}
-    let(:current_user) { stub_model(User, :role? => true) }
+    
     before(:each) do
-      assign(:user, user)  
-      assign(:current_user, current_user)
+      user.stub(:new_record? => false)
     end 
     
-    def do_render
-      render "form", :user => user, :button_label => "Update User"
-    end
-    
     it "should render a form to edit a user account" do
-      do_render 
+      do_render("Update User")
       form(:action => admin_user_path(user)) do |form|
         form.should have_selector("input", :type => "submit", :value => "Update User")   
       end
     end
     
     it "should not have field to update Partners UID" do
-      do_render
+      do_render("Update User")
       form(:action => admin_user_path(user)) do |form|
         rendered.should_not have_selector("input", :type => "text", :name => "user[puid]")
       end
@@ -92,7 +96,7 @@ describe "admin/users/_form.html.haml" do
     
     it "should show a field to update the username" do      
       user.stub(:username => "foobar")   
-      do_render
+      do_render("Update User")
       form(:action => admin_user_path(user)) do |form|
         form.should have_selector("input", :type => "text", :name => "user[username]", :value => "#{user.username}")
       end
@@ -100,7 +104,7 @@ describe "admin/users/_form.html.haml" do
 
     it "should show a field to enter an email address" do  
       user.stub(:email => "foobar@example.com")
-      do_render 
+      do_render("Update User")
       form(:action => admin_user_path(user)) do |form|
         form.should have_selector("input", :type => "email", :name => "user[email]", :value => "#{user.email}")
       end
@@ -108,7 +112,7 @@ describe "admin/users/_form.html.haml" do
 
     it "should show radio button to set user role if user is an admin" do 
       user.stub(:role => "admin")
-      do_render 
+      do_render("Update User")
       form(:action => admin_user_path(user)) do |form|
         form.should have_selector("input", :type => "radio", :name => "user[role]", :value => "editor")
         form.should have_selector("input", :type => "radio", :name => "user[role]", :checked => "checked", :value => "admin")   
@@ -117,7 +121,7 @@ describe "admin/users/_form.html.haml" do
     
     it "should not show the radio buttons if the user is not an admin" do 
       current_user.stub(:role? => false)
-      do_render
+      do_render("Update User")
       form(:action => admin_user_path(user)) do |form|
         form.should_not have_selector("input", :type => "radio", :name => "user[role]", :value => "editor")
         form.should_not have_selector("input", :type => "radio", :name => "user[role]", :value => "admin")   
@@ -125,7 +129,7 @@ describe "admin/users/_form.html.haml" do
     end
     
     it "should show a cancel link" do
-      do_render
+      do_render("Update User")
       form(:action => admin_user_path(user)) do |form|
         form.should have_selector("a.cancel", :href => admin_users_path)
       end
