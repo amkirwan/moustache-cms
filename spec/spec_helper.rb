0c760d6113f735beab5a14157e05a073118543fa
Anthony Kirwan <amkirwan@gmail.com>
2011-03-18 17:40:47 -0400

    spork config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,7 @@ Spork.prefork do
   require 'rspec/rails'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f} 
+  # in spec/support/ and its subdirectories. 
   Dir[Rails.root.join("spec/spec_helpers/*.rb")].each {|f| require f}  
 
   RSpec.configure do |config|
@@ -45,6 +44,7 @@ Spork.prefork do
 end
 
 Spork.each_run do
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
   load "#{Rails.root}/config/routes.rb"
   Dir["#{Rails.root}/app/**/*.rb"].each { |f| load f }
 end
