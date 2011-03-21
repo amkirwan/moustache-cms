658fcefb52424685d7b23fe1596ae956c9841be3
Anthony Kirwan <amkirwan@gmail.com>
2011-03-21 14:35:35 -0400

    removed unnecessary before_each block

--- spec/models/current_state_spec.rb
+++ spec/models/current_state_spec.rb
@@ -2,10 +2,6 @@
 require File.expand_path(File.dirname(__FILE__) + '../../spec_helper')
 
 describe CurrentState do   
-  before(:each) do
-    @current_state = CurrentState.make
-  end
-  
   context "CurrentState#all" do
     it "should return all the statuses" do
       CurrentState.all.count.should == 2
