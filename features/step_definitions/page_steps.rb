a7fcf9ca9e611edcd16b20e69b75943c27e437bb
Anthony Kirwan <amkirwan@gmail.com>
2011-03-31 23:29:53 -0400

    chaned blueprint to factory

--- features/step_definitions/page_steps.rb
+++ features/step_definitions/page_steps.rb
@@ -1,7 +1,7 @@
 Given /^these pages exist with current state$/ do |table|
   root_page = Factory(:root_page)
   table.hashes.each do |hash|
-    Factory(:no_root_page, :parent_id => root_page.id, :title => hash[:title], :current_state => CurrentState.make(:name => hash[:status]))
+    Factory(:no_root_page, :parent_id => root_page.id, :title => hash[:title], :current_state => Factory.build(:current_state, :name => hash[:status]))
   end
 end
 
