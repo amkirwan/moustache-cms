f9304413069d99470772e7bfe601cc352de0d4c3
Anthony Kirwan <amkirwan@gmail.com>
2011-04-26 15:47:19 -0400

    added site name

--- spec/views/layouts/admin/admin.html.haml_spec.rb
+++ spec/views/layouts/admin/admin.html.haml_spec.rb
@@ -1,10 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
 
 describe "layouts/admin/admin.html.haml" do
+  let(:site) { Factory(:site) }
   let(:user) { mock_model(User).as_null_object }  
-  before(:each) do  
+  before(:each) do
+    assign(:site, site)  
     ability_init
   end          
+  
+  it "should display the humanized site name" do
+    
+    render
+    rendered.should contain("#{site.name.humanize} Site")
+  end
   
   it "should display Users link when the user has the correct roles to manage them" do 
     @ability.can :manage, User     
