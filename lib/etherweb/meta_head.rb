366c107248b43a2db6de9d5534eae33c1baea817
Anthony Kirwan <amkirwan@gmail.com>
2011-05-03 23:11:58 -0400

    added specific mustache tags for meta title, keywords, description

--- lib/etherweb/meta_head.rb
+++ lib/etherweb/meta_head.rb
@@ -1,6 +1,18 @@
 module Etherweb
   module MetaHead
     
+    def meta_title
+      %(<meta name="title" content="#{@page.meta_data["title"]}")
+    end
+    
+    def meta_keywords
+      %(<meta name="keywords" content="#{@page.meta_data["keywords"]}")
+    end
+    
+    def meta_description
+      %(<meta name="description" content="#{@page.meta_data["description"]}")
+    end
+    
     def meta_data
       meta_data = ""
       @page.meta_data.each_pair do |k, v|
--- spec/lib/etherweb/meta_head_shared_examples.rb
+++ spec/lib/etherweb/meta_head_shared_examples.rb
@@ -2,6 +2,18 @@ require "spec_helper"
 
 shared_examples_for "MetaHead" do
   
+  it "should return the meta title" do
+    @cmsp.meta_title.should == %(<meta name="title" content="#{@page.meta_data["title"]}")
+  end
+  
+  it "should return the meta keywords" do
+    @cmsp.meta_keywords.should == %(<meta name="keywords" content="#{@page.meta_data["keywords"]}")
+  end
+  
+  it "should return the meta description" do
+    @cmsp.meta_description.should == %(<meta name="description" content="#{@page.meta_data["description"]}")
+  end
+  
   it "should set the meta fields for the page" do
     @cmsp.meta_data.should == %(<meta name="title" content="#{@page.meta_data["title"]}">\n<meta name="keywords" content="#{@page.meta_data["keywords"]}">\n<meta name="description" content="#{@page.meta_data["description"]}">\n)
   end
