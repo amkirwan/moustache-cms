d6e1fa5a11210d02a9d17fa083aaa8c005bf6aff
Anthony Kirwan <amkirwan@gmail.com>
2011-05-10 12:53:05 -0400

    generated carrierwave

--- app/models/media_file.rb
+++ app/models/media_file.rb
@@ -1,16 +1,17 @@
 class MediaFile
   include Mongoid::Document
   
+  attr_accessible :name, :description, :alt_text, :media_asset
+  
   # -- Fields --------------- 
   field :name
   field :description
   field :alt_text
-  field :url
-  field :current_path
-  field :content_type
+  field :media_asset
   
   # -- Associations -------------
   belongs_to :created_by, :class_name => "User"
   belongs_to :updated_by, :class_name => "User"
   belongs_to :site
+  mount_uploader :media_asset, MediaAssetUploader
 end
