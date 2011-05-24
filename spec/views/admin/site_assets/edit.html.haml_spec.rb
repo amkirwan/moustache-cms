08e2feabc6b07f6ddffa8fdfec275aeca41f5d94
Anthony Kirwan <amkirwan@gmail.com>
2011-05-24 16:37:58 -0400

    all view specs passing for site_asset change from asset

--- spec/views/admin/site_assets/edit.html.haml_spec.rb
+++ spec/views/admin/site_assets/edit.html.haml_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
-describe "admin/media_files/edit.html.haml" do
-  let(:media_file) { stub_model(MediaFile, :name => "foobar") }
+describe "admin/site_assets/edit.html.haml" do
+  let(:site_asset) { stub_model(SiteAsset, :name => "foobar") }
   let(:current_user) { stub_model(User, :role? => true) }
   
   before(:each) do
-    assign(:media_file, media_file)
+    assign(:site_asset, site_asset)
     assign(:current_user, current_user)
   end
 
-  it "renders the edit media_file form" do
+  it "renders the edit site_asset form" do
     render
-    rendered.should have_selector("h3", :content => "Edit Media File #{media_file.name.humanize}")
+    rendered.should have_selector("h3", :content => "Edit Asset #{site_asset.name.humanize}")
   end
   
   it "should render the form partial to edit the media file" do
     render
-    view.should render_template(:partial => "form", :locals => {:media_file => media_file, :button_label => "Update Media File" })
+    view.should render_template(:partial => "form", :locals => {:site_asset => site_asset, :button_label => "Update Asset" })
   end
 end
--- spec/views/admin/site_assets/new.html.haml_spec.rb
+++ spec/views/admin/site_assets/new.html.haml_spec.rb
@@ -1,22 +1,22 @@
 # NEW media_files
 require 'spec_helper'
 
-describe "admin/media_files/new.html.haml" do
-  let(:media_file) { stub_model(MediaFile) }
+describe "admin/site_assets/new.html.haml" do
+  let(:site_asset) { stub_model(SiteAsset) }
   let(:current_user) { stub_model(User, :role? => true) }
   
   before(:each) do
-    assign(:media_file, media_file.as_new_record)
+    assign(:site_asset, site_asset.as_new_record)
     assign(:current_user, current_user)
   end
 
-  it "renders new media_file form" do
+  it "renders new site_asset form" do
     render
-    rendered.should have_selector("h3", :content => "Create New Media")
+    rendered.should have_selector("h3", :content => "Create New Asset")
   end
   
   it "should render the form partial" do
     render
-    view.should render_template(:partial => "form", :locals => { :media_file => media_file, :button_label => "Save Media" })
+    view.should render_template(:partial => "form", :locals => { :site_asset => site_asset, :button_label => "Save Asset" })
   end
 end
