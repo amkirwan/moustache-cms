09b85cc36fe89007ecea637b46b9312a5be12461
Anthony Kirwan <amkirwan@gmail.com>
2011-05-27 15:20:41 -0400

    changed to use instance variables css_files, js_files, and images

--- spec/views/admin/theme_assets/index.html.haml_spec.rb
+++ spec/views/admin/theme_assets/index.html.haml_spec.rb
@@ -3,19 +3,22 @@ require 'spec_helper'
 
 describe "admin/theme_assets/index.html.haml" do
   let(:theme_assets) { [stub_model(ThemeAsset, :name => "foobar")] }
+  let(:css_files) { [ stub_model(ThemeAsset, :name => "foobar")] }
+  let(:js_files) { [ stub_model(ThemeAsset, :name => "foobar")] }
+  let(:images) { [ stub_model(ThemeAsset, :name => "foobar")] }
   
   before(:each) do
     assign(:theme_assets, theme_assets)
-    theme_assets.stub(:css_files => theme_assets)
-    theme_assets.stub(:js_files => theme_assets)
-    theme_assets.stub(:images => theme_assets)
+    assign(:css_files, css_files)
+    assign(:js_files, js_files)
+    assign(:images, images)
   end
   
   describe "css section" do
     
     it "should make the css filename a clickable link to edit" do
       render 
-      theme_assets.css_files.each do |css|
+      css_files.each do |css|
         rendered.should have_selector("li##{css.name}") do |li|
           li.should have_selector("a", :content => "#{css.name}", :href => edit_admin_theme_asset_path(css))
         end
@@ -24,7 +27,7 @@ describe "admin/theme_assets/index.html.haml" do
     
     it "should render a delete button to destroy the css file" do
       render
-      theme_assets.css_files.each do |css|
+      css_files.each do |css|
         rendered.should have_selector("li##{css.name}") do |li|
           li.should have_selector("input", :value => "delete")
         end
@@ -36,7 +39,7 @@ describe "admin/theme_assets/index.html.haml" do
 
     it "should make the javascript filename a clickable link to edit" do
       render 
-      theme_assets.js_files.each do |js|
+      js_files.each do |js|
         rendered.should have_selector("li##{js.name}") do |li|
           li.should have_selector("a", :content => "#{js.name}", :href => edit_admin_theme_asset_path(js))
         end
@@ -45,7 +48,7 @@ describe "admin/theme_assets/index.html.haml" do
     
     it "should render a delete button to destroy the javascript file" do
       render
-      theme_assets.js_files.each do |js|
+      js_files.each do |js|
         rendered.should have_selector("li##{js.name}") do |li|
           li.should have_selector("input", :value => "delete")
         end
@@ -57,7 +60,7 @@ describe "admin/theme_assets/index.html.haml" do
     
     it "should make the image filename a clickable link to edit" do
       render 
-      theme_assets.images.each do |image|
+      images.each do |image|
         rendered.should have_selector("li##{image.name}") do |li|
           li.should have_selector("a", :content => "#{image.name}", :href => edit_admin_theme_asset_path(image))
         end
@@ -66,7 +69,7 @@ describe "admin/theme_assets/index.html.haml" do
     
     it "should render a delete button to destroy the image file" do
       render
-      theme_assets.images.each do |image|
+      images.each do |image|
         rendered.should have_selector("li##{image.name}") do |li|
           li.should have_selector("input", :value => "delete")
         end
