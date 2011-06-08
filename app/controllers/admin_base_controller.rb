4b0f9e7d09d51e88c4aa0d66804bdc75ab7b238e
Anthony M Kirwan <amkirwan@gmail.com>
2011-06-08 18:27:42 -0400

    changed current_user assignment to ||=

--- app/controllers/admin_base_controller.rb
+++ app/controllers/admin_base_controller.rb
@@ -18,7 +18,7 @@ class AdminBaseController < ApplicationController
   protected 
     def current_user
       current_site if @current_site.nil?
-      @current_user = User.where(:puid => session[:cas_user], :site_id => @current_site.id).first
+      @current_user ||= User.where(:puid => session[:cas_user], :site_id => @current_site.id).first
     end  
     
     def current_user?(user)
