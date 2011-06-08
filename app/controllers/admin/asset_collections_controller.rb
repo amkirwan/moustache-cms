a0c66ac026219045257b9880d5732eac85dba194
Anthony M Kirwan <amkirwan@gmail.com>
2011-06-08 18:29:07 -0400

    all asset_collection controller spec methods passing

--- app/controllers/admin/asset_collections_controller.rb
+++ app/controllers/admin/asset_collections_controller.rb
@@ -1,5 +1,49 @@
-class Admin::AssetCollectionsController < ApplicationController
+class Admin::AssetCollectionsController < AdminBaseController   
   
+  # GET /admin/asset_collections
   def index
+  end
+  
+  # GET /admin/asset_collections/1
+  def show
+  end
+  
+  # GET /admin/asset_collections/new
+  def new
+  end
+  
+  #GET /admin/asset_collections/1/edit
+  def edit
+  end
+  
+  #POST /admin/asset_collections
+  def create
+    created_updated_by_for @asset_collection
+    @asset_collection.site = @current_site
+    if @asset_collection.save  
+      flash[:notice] = "Successfully created the asset collection #{@asset_collection.name}"
+      redirect_to admin_asset_collections_path
+    else
+      render :new
+    end
+  end
+  
+  #PUT /admin/asset_collections/1
+  def update
+    @asset_collection.updated_by = @current_user
+    if @asset_collection.update_attributes(params[:asset_collection])
+      flash[:notice] = "Successfully updated the asset #{@asset_collection.name}"
+      redirect_to admin_asset_collection_path
+    else
+      render :edit
+    end
+  end
+  
+  #DELETE /admin/asset_collections/1
+  def destroy
+    if @asset_collection.destroy
+      flash[:notice] = "Successfully deleted the asset collection #{@asset_collection.name}"
+      redirect_to admin_asset_collections_path
+    end
   end
 end
--- spec/controllers/admin/asset_collections_controller_spec.rb
+++ spec/controllers/admin/asset_collections_controller_spec.rb
@@ -11,4 +11,263 @@ describe Admin::AssetCollectionsController do
     stub_c_site_c_user(site, current_user)
   end
 
+
+  describe "GET index" do
+    let(:asset_collections) { [asset_collection] }
+    
+    before(:each) do
+      AssetCollection.stub(:accessible_by).and_return(asset_collections)
+    end
+    
+    def do_get
+      get :index      
+    end
+    
+    it "should receive AssetCollection#accessible_by and return asset_collection" do
+      AssetCollection.should_receive(:accessible_by).and_return(asset_collections)
+      do_get
+    end
+    
+    it "should assign asset_collections" do
+      do_get
+      assigns(:asset_collections).should == asset_collections
+    end
+    
+    it "should render in the index template" do
+      do_get
+      response.should render_template("admin/asset_collections/index")
+    end
+  end
+  
+  describe "GET show" do
+    before(:each) do
+      AssetCollection.stub(:find).and_return(asset_collection)
+    end
+    
+    def do_get
+      get :show, "id" => asset_collection.to_param
+    end
+    
+    it "should receive AssetCollection#find" do
+      AssetCollection.should_receive(:find).and_return(asset_collection)
+      do_get
+    end
+    
+    it "should assign asset_collection" do
+      do_get
+      assigns(:asset_collection).should == asset_collection
+    end
+    
+    it "should render the show template" do
+      do_get
+      response.should render_template("admin/asset_collections/show")
+    end    
+  end
+  
+  describe "GET new" do
+    before(:each) do
+      AssetCollection.stub(:new).and_return(asset_collection.as_new_record)
+    end
+    
+    def do_get
+      get :new
+    end
+
+    it "should receive AssetCollection.new" do
+      AssetCollection.should_receive(:new).and_return(asset_collection)
+      do_get
+    end
+    
+    it "should assign asset_collection" do
+      do_get
+      assigns(:asset_collection).should == asset_collection
+    end
+    
+    it "should render the new template" do
+      do_get
+      response.should render_template("admin/asset_collections/new")
+    end    
+  end
+  
+  describe "GET edit" do
+    before(:each) do
+      AssetCollection.stub(:find).with(asset_collection.to_param).and_return(asset_collection)
+    end
+    
+    def do_get
+      puts "#{asset_collection.to_param}"
+      get :edit, "id" => asset_collection.to_param
+    end
+    
+    it "should receive AssetCollection.find" do
+      AssetCollection.should_receive(:find).with(asset_collection.to_param).and_return(asset_collection)
+      do_get
+    end
+    
+    it "should assign asset_collection" do
+      do_get
+      assigns(:asset_collection).should == asset_collection
+    end
+    
+    it "should render the edit template" do
+      do_get
+      response.should render_template("admin/asset_collections/edit")
+    end
+  end
+  
+  describe "POST create" do
+    let(:params) {{ "asset_collection" => { "name" => "foobar" }}}
+    
+    before(:each) do
+      asset_collection.as_new_record
+      AssetCollection.stub(:new).and_return(asset_collection)
+    end
+    
+    def do_post(post_params=params)
+      post :create, post_params
+    end
+    
+    it "should receive AssetCollection.new" do
+      AssetCollection.should_receive(:new).and_return(asset_collection)
+      do_post
+    end
+    
+    it "should assign asset_collection" do
+      do_post
+      assigns(:asset_collection).should == asset_collection
+    end
+    
+    it "should assign created_by and updated by to the current user" do
+      controller.should_receive(:created_updated_by_for).with(asset_collection)
+      do_post
+    end
+    
+    it "should assign the current_site" do
+      asset_collection.should_receive(:site=).with(site)
+      do_post
+    end
+    
+    context "with valid params" do
+      it "should receive save and return true" do
+        asset_collection.should_receive(:save).and_return(true)
+        do_post
+      end
+      
+      it "should assign the flash message" do
+        do_post
+        flash[:notice].should == "Successfully created the asset collection #{asset_collection.name}"
+      end
+      
+      it "should redirect to the index template" do
+        do_post
+        response.should redirect_to admin_asset_collections_path
+      end
+    end
+    
+    context "with invalid params" do
+      before(:each) do
+        asset_collection.stub(:save).and_return(false)
+      end
+      
+      it "should receive save and return false" do
+        asset_collection.should_receive(:save).and_return(false)
+        do_post  
+      end
+      
+      it "should render the new template" do
+        do_post
+        response.should render_template("admin/asset_collections/new")
+      end
+    end
+  end
+  
+  describe "PUT update" do
+    let(:params) { { "id" => asset_collection.to_param, "asset_collection" => { "name" => "foobar" } } }
+    
+    before(:each) do
+      AssetCollection.stub(:find).and_return(asset_collection)
+    end
+    
+    def do_put(put_params=params)
+      post :update, put_params
+    end
+    
+    it "should receive AssetCollection.find" do
+      AssetCollection.should_receive(:find).and_return(asset_collection)
+      do_put
+    end
+    
+    it "should assign asset_collection" do
+      do_put
+      assigns(:asset_collection).should == asset_collection
+    end
+    
+    it "should update updated_by attribute" do
+      asset_collection.should_receive(:updated_by=).with(current_user)
+      do_put
+    end
+    
+    context "with valid params" do
+      it "should receive update_attributes and update" do
+        asset_collection.should_receive(:update_attributes).with(params["asset_collection"]).and_return(true)
+        do_put
+      end
+      
+      it "should assign the flash message" do
+        do_put
+        flash[:notice].should == "Successfully updated the asset #{asset_collection.name}"
+      end
+      
+      it "should redirect to index template" do
+        do_put
+        response.should redirect_to admin_asset_collection_path
+      end
+    end
+    
+    context "with invalid params" do
+      before(:each) do
+        asset_collection.should_receive(:update_attributes).and_return(false)
+      end
+      
+      it "should render the edit template" do
+        do_put
+        response.should render_template("admin/asset_collections/edit")
+      end
+    end
+  end
+  
+  describe "DELETE destroy" do
+    before(:each) do
+      AssetCollection.stub(:find).and_return(asset_collection)
+    end
+    
+    def do_delete
+      delete :destroy, "id" => asset_collection.to_param
+    end
+    
+    it "should receive AssetCollection.find" do
+      AssetCollection.should_receive(:find).with(asset_collection.to_param).and_return(asset_collection)
+      do_delete
+    end
+    
+    it "should assign asset_collection" do
+      do_delete
+      assigns(:asset_colletion).should asset_collection
+    end
+    
+    it "should receive destroy and return true" do
+      asset_collection.should_receive(:destroy).and_return(true)
+      do_delete
+    end
+    
+    it "should assign the flash message" do
+      do_delete
+      flash[:notice].should == "Successfully deleted the asset collection #{asset_collection.name}"
+    end
+    
+    it "should redirect to the index path" do
+      do_delete
+      response.should redirect_to admin_asset_collections_path
+    end
+  end
 end
