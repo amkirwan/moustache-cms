4814b1905e698bd2a6dd81c56a26b1a63ff73798
Anthony M Kirwan <amkirwan@gmail.com>
2011-06-24 23:01:11 -0400

    fixed problems with haml spacing

--- lib/etherweb/mustache/navigation.rb
+++ lib/etherweb/mustache/navigation.rb
@@ -6,9 +6,9 @@ module Etherweb
       def nav_child_pages(css_class="nav") 
 template = %{
 %ul{:class => css_class}
-- parent_page.children.each do |page|
+  - parent_page.children.each do |page|
   %li 
-    - href = "http://#{@request.host.downcase}" + page.full_path
+  - href = "http://#{@request.host.downcase}" + page.full_path
     %a{:id => page.title.downcase.gsub(/\s/, '_'), :title => page.title, :href => href }= page.title
 }        
         engine = gen_haml(template.strip!)
@@ -38,9 +38,9 @@ template = %{
 template = %{
 %ul{:class => "nav nav_#{page_name}"}
   - parent_page.children.each do |page|
-    %li
-      - href = "http://#{@request.host.downcase}" + page.full_path
-      %a{:id => page.title.downcase.gsub(/\s/, '_'), :title => page.title, :href => href }= page.title
+  %li
+  - href = "http://#{@request.host.downcase}" + page.full_path
+    %a{:id => page.title.downcase.gsub(/\s/, '_'), :title => page.title, :href => href }= page.title
 }
         engine = gen_haml(template.strip!)
         engine.render(Object.new, {:page_name => page_name, :parent_page => @current_site.page_by_name(page_name)})
@@ -51,9 +51,9 @@ template = %{
 template = %{
 %ul{:class => "nav nav_#{page_name}"}
   - sibling.siblings_and_self.each do |page|
-    %li
-      - href = "http://#{@request.host.downcase}" + page.full_path
-      %a{:id => page.title.downcase.gsub(/\s/, '_'), :title => page.title, :href => href }= page.title   
+  %li
+  - href = "http://#{@request.host.downcase}" + page.full_path
+    %a{:id => page.title.downcase.gsub(/\s/, '_'), :title => page.title, :href => href }= page.title   
 }
 
         engine = gen_haml(template.strip!)
