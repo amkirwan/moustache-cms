2a9da175de2b51a799f365454861f1fd21ee9f51
Anthony Kirwan <amkirwan@gmail.com>
2011-08-12 16:33:06 -0400

    proper indenting and vim mode working in ace texteditor for layout

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
-  var editor = ace.edit("layout_content");  
+  window.editor = ace.edit("ace_editor");  
+
+  // configure editor
   var HTMLMode = require("ace/mode/html").Mode;
-  editor.getSession().setMode(new HTMLMode());
-  editor.setTheme("ace/theme/clouds");
+  window.editor.getSession().setMode(new HTMLMode());
+  window.editor.setTheme("ace/theme/textmate");
+
+  window.editor.keyBinding.setKeyboardHandler('ace/keyboard/keybinding/vim'.Vim);
+  window.editor.getSession().setTabSize(2);
+  window.editor.getSession().setUseSoftTabs(true);
+
+  var theContent = $('textarea#layout_content').val();
+  
+  window.editor.getSession().setValue(theContent);
+
+  $('.ace_editor').submit(function() {
+    var content = window.editor.getSession().getValue();
+    $('textarea#layout_content').val(content);
+  });
 });
