4185de981e5bddaf0bc00bf817165b50a4b0627e
Anthony Kirwan <amkirwan@gmail.com>
2011-08-12 00:19:52 -0400

    dropped codemirror for ace editor

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
+$(document).ready(function(){
+  var editor = ace.edit("layout_content");  
+  var HTMLMode = require("ace/mode/html").Mode;
+  editor.getSession().setMode(new HTMLMode());
+  editor.setTheme("ace/theme/clouds");
+});
