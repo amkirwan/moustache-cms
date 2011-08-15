dbe02e67e0fedadba9d1d67f1c4d27f3f104b85c
Anthony Kirwan <amkirwan@gmail.com>
2011-08-15 14:54:02 -0400

    changed editor to html anded html namespace to wrapping div

--- public/javascripts/markitup/sets/html/set.js
+++ public/javascripts/markitup/sets/html/set.js
@@ -10,6 +10,7 @@
 // Basic set. Feel free to add more tags
 // ----------------------------------------------------------------------------
 mySettings = {
+  nameSpace: 'html',
 	onShiftEnter:	{keepDefault:false, replaceWith:'<br />\n'},
 	onCtrlEnter:	{keepDefault:false, openWith:'\n<p>', closeWith:'</p>\n'},
 	onTab:			{keepDefault:false, openWith:'	 '},
