9a5655865d30ca116c28a4cac84bdbbc4887513c
Anthony Kirwan <amkirwan@gmail.com>
2011-09-22 16:18:15 -0400

    added validations to meta_tag

--- app/models/meta_tag.rb
+++ app/models/meta_tag.rb
@@ -8,4 +8,11 @@ class MetaTag
   # -- Association --
   embedded_in :meta_taggable, :polymorphic => true
 
+  validates :name,
+            :presence => true,
+            :uniqueness => {:message => "Cannot have duplicate meta tag names"}
+
+  validates :content,
+            :presence => true
+
 end
