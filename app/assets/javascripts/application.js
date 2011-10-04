29d4a908966dde67d48c748770032842c09651b2
Anthony Kirwan <amkirwan@gmail.com>
2011-10-04 12:47:17 -0400

    added fade effect to flash_notice

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,12 @@
 //= require jquery_ujs
 //= require markitup_selector
 //= require admin/pages
+//= require_self
+
+jQuery(function() {
+  return $(document).ready(function() {
+    $('#flash_notice_wrapper').delay(3000).fadeToggle('slow', 'linear', function() {
+      $(this).remove()
+    }); 
+  });
+});
