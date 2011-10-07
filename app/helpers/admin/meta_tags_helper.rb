98cdce24e766eef690e3b2e2021e798313f3ac49
Anthony Kirwan <amkirwan@gmail.com>
2011-10-07 14:56:35 -0400

    added helper for adding a meta_tag to a page

--- app/helpers/admin/meta_tags_helper.rb
+++ app/helpers/admin/meta_tags_helper.rb
@@ -1,2 +1,13 @@
 module Admin::MetaTagsHelper
+
+  def add_meta_tag(message)
+    if @page.new_record?
+      content_tag :p do
+        content_tag :i, message
+      end
+    else
+      link_to "Add Meta Tag", [:new, :admin, @page, :meta_tag], :remote => :true
+    end
+  end
+
 end
