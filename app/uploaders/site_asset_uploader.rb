41f49f4379e5e32bade24f08e986b9d3a87cebee
Anthony Kirwan <amkirwan@gmail.com>
2011-10-12 21:58:45 -0400

    theme_asset partialized form

--- app/uploaders/site_asset_uploader.rb
+++ app/uploaders/site_asset_uploader.rb
@@ -32,7 +32,7 @@ class SiteAssetUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
    def extension_white_list
-     %w(jpg jpeg gif png pdf mp4 m4v ogv webm flv)
+     %w(jpg jpeg gif png pdf mp4 m4v ogv webm flv otf)
    end
     
   # Override the filename of the uploaded files:
