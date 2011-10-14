31f58192e961dc0862d2724bb6d9b788a54b8d6a
Anthony Kirwan <amkirwan@gmail.com>
2011-10-14 18:18:56 -0400

    added additional subdomain to site settings view

--- app/helpers/admin/current_site_helper.rb
+++ app/helpers/admin/current_site_helper.rb
@@ -1,5 +1,7 @@
 module Admin::CurrentSiteHelper
 
-  def manage_domain
+  def manage_domains
+    domains = @current_site.domains.delete_if { |x| x == "#{@current_site.full_subdomain}" }
+    render :partial => 'admin/current_site/domains', :object => domains
   end
 end
