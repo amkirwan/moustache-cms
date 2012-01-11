dd03f9e4b3847811b73cb7e168b747b524748078
Anthony Kirwan <amkirwan@gmail.com>
2012-01-11 14:59:37 -0500

    removed extra :// from page_full_path_with_request

--- lib/handlebar_cms/mustache/cms_page.rb
+++ lib/handlebar_cms/mustache/cms_page.rb
@@ -5,7 +5,7 @@ class TagHelper
   include ActionView::Helpers
 
   def page_full_path_with_request(request, page)
-    "#{request.protocol}://#{request.host.downcase}" + page.full_path
+    "#{request.protocol}#{request.host.downcase}" + page.full_path
   end
 end
 
