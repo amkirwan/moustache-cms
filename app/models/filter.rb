193fb09292d5afd2244b0ab2941a184e21cadbbf
Anthony Kirwan <amkirwan@gmail.com>
2012-01-27 17:23:58 -0500

    removed haml filter for security

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -21,10 +21,6 @@ class Filter
     @filters
   end
   
-  def self.all_page_filters
-    @filters_page
-  end
-  
   def self.find(id)
     filter = @filters.find { |filter| filter.id == id.to_s.downcase }
     filter unless filter.nil?
@@ -39,13 +35,6 @@ class Filter
   @filters = [
     Filter.new(:name => "markdown"),
     Filter.new(:name => "textile"),
-    Filter.new(:name => "haml"),
     Filter.new(:name => "html")
   ]
-  
-  @filters_page = [
-    Filter.new(:name => "markdown"),
-    Filter.new(:name => "textile"),
-    Filter.new(:name => "html")
-    ]
 end
