befe5111e3edb152e1ee44112952543695161296
Anthony Kirwan <amkirwan@gmail.com>
2012-01-31 14:16:21 -0500

    added ace editor to layouts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,38 +15,34 @@
 //= require site_asset_plupload
 //= require nested/jquery.ui.nestedSortable
 //= require_self
-//= require externals
 
-jQuery(function() {
-  return $(document).ready(function() {
-    $('#flash_notice_wrapper').delay(3000).fadeToggle('slow', 'linear', function() {
-      $(this).remove()
-    }); 
+$(document).ready(function() {
+  $('#flash_notice_wrapper').delay(3000).fadeToggle('slow', 'linear', function() {
+    $(this).remove()
+  }); 
 
-    $('.foldable legend').mouseup(function() {
-      var legend = $(this);
-      legend.siblings().slideToggle('slow', 'linear', function() {
-        var arrow = legend.children().first();
-        if (arrow.hasClass('rotate')) {
-          arrow.removeClass('rotate');
-        } else { 
-          arrow.addClass('rotate');
-        }
-      });
+  $('.foldable legend').mouseup(function() {
+    var legend = $(this);
+    legend.siblings().slideToggle('slow', 'linear', function() {
+      var arrow = legend.children().first();
+      if (arrow.hasClass('rotate')) {
+        arrow.removeClass('rotate');
+      } else { 
+        arrow.addClass('rotate');
+      }
     });
-
-    $('fieldset.page_parts').mouseup(function() {
-      var legend = $(this);
-      legend.siblings('div#page_parts_wrapper').slideToggle('slow', 'linear', function() {
-        var arrow = legend.children().first();
-        if (arrow.hasClass('rotate')) {
-          arrow.removeClass('rotate');
-        } else { 
-          arrow.addClass('rotate');
-        }
-      });
-    });
-
   });
-});
 
+  $('fieldset.page_parts').mouseup(function() {
+    var legend = $(this);
+    legend.siblings('div#page_parts_wrapper').slideToggle('slow', 'linear', function() {
+      var arrow = legend.children().first();
+      if (arrow.hasClass('rotate')) {
+        arrow.removeClass('rotate');
+      } else { 
+        arrow.addClass('rotate');
+      }
+    });
+  });
+
+});
--- vendor/assets/javascripts/externals.js
+++ vendor/assets/javascripts/externals.js
@@ -1,1 +1,2 @@
 //= require_directory ./markitup
+
