a323fa34b6b043103801d06e1116ea43da45f5db
Anthony Kirwan <amkirwan@gmail.com>
2012-02-15 15:29:42 -0500

    fixed session store name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-HandlebarCms::Application.config.session_store :cookie_store, :key => '_handlebar_csm_session'
+HandlebarCms::Application.config.session_store :cookie_store, :key => '_handlebar_cms_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
