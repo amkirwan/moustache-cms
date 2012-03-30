66f3f931c577bcc4f58b4da3a84ad822023892ae
Anthony Kirwan <amkirwan@gmail.com>
2012-03-30 15:31:37 -0400

    added spec from theme collection

--- spec/models/theme_collection_spec.rb
+++ spec/models/theme_collection_spec.rb
@@ -1,4 +1,55 @@
 require 'spec_helper'
 
 describe ThemeCollection do
+  let(:site) { Factory(:site) }
+
+  before(:each) do
+    @theme_collection = Factory(:theme_collection, :site => site) 
+  end
+
+  # -- Associations ---
+  describe "associations" do
+    it "should belong to a site" do
+      @theme_collection.should belong_to(:site)
+    end
+
+    it "should belong to a created_by" do
+      @theme_collection.should belong_to(:created_by).of_type(User) 
+    end
+
+    it "should belong to updated_by" do
+      @theme_collection.should belong_to(:updated_by).of_type(User)
+    end
+
+    it "should embed many theme_assets" do
+      @theme_collection.should embed_many(:theme_assets)
+    end
+  end
+
+  # -- Validations ---
+  describe "Validations" do
+    it "should be a valid theme collection" do
+      @theme_collection.should be_valid
+    end       
+
+    it "should not be valid without a name" do
+      @theme_collection.name = nil
+      @theme_collection.should_not be_valid
+    end
+
+    it "should not be valid without a site_id" do
+      @theme_collection.site_id = nil
+      @theme_collection.should_not be_valid
+    end
+
+    it "should not be valid without created_by_id" do
+      @theme_collection.created_by_id = nil
+      @theme_collection.should_not be_valid   
+    end
+
+    it "should not be valid without updated_bY_id" do
+      @theme_collection.updated_by_id = nil
+      @theme_collection.should_not be_valid
+    end
+  end
 end
