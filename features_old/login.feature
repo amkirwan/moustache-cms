546e3be97adce12c0845ba31cbeffec9d2e149b6
Anthony Kirwan <amkirwan@gmail.com>
2012-04-07 21:32:05 -0400

    removed old features

--- features_old/login.feature
+++
@@ -1,6 +0,0 @@
-Feature: Login User
-
-  Scenario: Login to admin page
-    Given I go to the admin login page
-    When I login as an admin
-    Then I should see "Signed in successfully"
--- features_old/step_definitions/tag_attrs_steps.rb
+++
@@ -1,9 +0,0 @@
-Given /^the asset "([^"]*)" with the custom tag attribute "([^"]*)" and a value of "([^"]*)"$/ do |asset, name, value|
-  When %{I go to the admin theme assets page}
-  And %{I follow "#{asset}"}
-  And %{I follow "Add Tag Attribute"}
-  And %{I fill in "tag_attr_name" with "#{name}"}
-  And %{I fill in "tag_attr_value" with "#{value}"}
-  And %{I press "Create Tag Attribute"}
-  Then %{I should now be editing the theme asset "theme_css"}
-end
