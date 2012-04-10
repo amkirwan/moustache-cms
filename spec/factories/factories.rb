a546ab082f706db4fe0823ff9c639faf00fba311
Anthony Kirwan <amkirwan@gmail.com>
2012-04-10 13:45:22 -0400

    in the factory add relation on parent side have 1..N of created_by and updated_by. Setting from child was not setting the attributes on the parent

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -6,9 +6,11 @@ def set_meta_tags(type)
     ]
 end
 
-def assign_created_updated_by(obj)
-  obj.created_by = FactoryGirl.build(:user, site_id: obj.site_id)
-  obj.updated_by = FactoryGirl.build(:user, site_id: obj.site_id)
+def assign_created_updated_by(obj, relation)
+  obj.created_by = created_by = FactoryGirl.create(:user, site_id: obj.site_id)
+  obj.updated_by = updated_by = FactoryGirl.create(:user, site_id: obj.site_id)
+  created_by.send(relation.to_s + '_created') << obj
+  updated_by.send(relation.to_s + '_updated') << obj
 end
 
 def page_associations(page)
@@ -16,7 +18,7 @@ def page_associations(page)
   page.current_state = FactoryGirl.build(:current_state)
   page.editors = [ FactoryGirl.build(:user, site_id: page.site_id) ]
   page.page_parts [ FactoryGirl.build(:page_part) ]
-  assign_created_updated_by(page)
+  assign_created_updated_by(page, :pages)
 end
 
 def article_associations(article) 
@@ -27,7 +29,7 @@ def article_associations(article)
 end
 
 def assign_asset_collection(asset_collection)
-  assign_created_updated_by(asset_collection)
+  assign_created_updated_by(asset_collection, :asset_collections)
 end
 
 FactoryGirl.define do
@@ -73,7 +75,7 @@ FactoryGirl.define do
     association :site, strategy: :build
     sequence(:name) { |n| "layout_#{n}" }
     content "Hello, World!"
-    after_build { |layout| assign_created_updated_by(layout) }
+    after_build { |layout| assign_created_updated_by(layout, :layouts) }
   end
 
   factory :current_state do
@@ -86,7 +88,7 @@ FactoryGirl.define do
     sequence(:name) { |n| "name_#{n}" }
     content "snippet content"
     filter_name "snippet filter"
-    after_build { |snippet| assign_created_updated_by(snippet) }
+    after_build { |snippet| assign_created_updated_by(snippet, :snippets) }
   end
 
   factory :page_type do 
@@ -124,14 +126,14 @@ FactoryGirl.define do
     lastname "baz"
     profile "this is the author profile"
     image { File.open("#{Rails.root}/spec/fixtures/assets/rails.png") }
-    after_build { |author| assign_created_updated_by(author) }
+    after_build { |author| assign_created_updated_by(author, :author) }
   end
 
   factory :article_collection do 
     association :site, strategy: :build
     sequence(:name) { |n| "name_#{n}" }
     editors {[ FactoryGirl.build(:user) ]}
-    after_build { |article_collection| assign_created_updated_by(article_collection) }
+    after_build { |article_collection| assign_created_updated_by(article_collection, :article_collections) }
   end
 
   factory :article do 
@@ -139,17 +141,21 @@ FactoryGirl.define do
     sequence(:title) { |n| "title_#{n}" }
     sequence(:slug) { |n| "slug_#{n}" }
     tags "article"
+    layout_id { FactoryGirl.build(:layout).id }
+    current_state { FactoryGirl.build(:current_state) }
     meta_tags { set_meta_tags('article') }
     content "article content"
+    authors { [FactoryGirl.build(:user)] }
     filter_name "published"
-    after_build { |article| assign_created_updated_by(article) }
+    article_collection { FactoryGirl.build(:article_collection) }
+    after_build { |article| assign_created_updated_by(article, :articles) }
   end
 
   factory :asset_collection do 
     association :site, strategy: :build
     sequence(:name) { |n| "name_#{n}" }
     site_assets { [ FactoryGirl.build(:site_asset) ] }
-    after_build { |asset_collection| assign_created_updated_by(asset_collection) }
+    after_build { |asset_collection| assign_created_updated_by(asset_collection, :asset_collections) }
   end
 
   factory :site_asset do 
@@ -180,6 +186,6 @@ FactoryGirl.define do
   factory :theme_collection do 
     association :site, strategy: :build
     sequence(:name) { |n| "name_#{n}" }
-    after_build { |theme_collection| assign_created_updated_by(theme_collection) }
+    after_build { |theme_collection| assign_created_updated_by(theme_collection, :theme_collections) }
   end
 end
