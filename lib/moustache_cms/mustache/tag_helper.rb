981817b24694af95e84f35852bad398eb53800e1
Anthony Kirwan <amkirwan@gmail.com>
2012-07-24 17:15:22 -0400

    fixed issue with links generating extra ://

--- lib/moustache_cms/mustache/tag_helper.rb
+++ lib/moustache_cms/mustache/tag_helper.rb
@@ -3,6 +3,6 @@ class TagHelper
   include ActionView::Helpers
 
   def page_full_path_with_request(request, page)
-    "#{request.protocol}://#{request.host.downcase}" + page.full_path
+    "#{request.protocol}#{request.host.downcase}" + page.full_path
   end
 end
