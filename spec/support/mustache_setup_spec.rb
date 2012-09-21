82a6ff3c52be02eb6cd47cb03b7e9e937df21139
Anthony Kirwan <amkirwan@gmail.com>
2012-09-20 23:00:38 -0400

    added shared context for mustache specs

---
+++ spec/support/mustache_setup_spec.rb
@@ -0,0 +1,21 @@
+shared_context "mustache page setup" do
+  let(:site) { FactoryGirl.create(:site)}
+  let(:user) { FactoryGirl.create(:user, :site => site) }
+  let(:layout) { FactoryGirl.create(:layout, :site => site, :created_by => user, :updated_by => user) }
+
+  before(:each) do
+    @controller = CmsSiteController.new
+    @page = FactoryGirl.create(:page, :title => "foobar", :site => site, :created_by => user, :updated_by => user)
+    @page.page_parts << FactoryGirl.build(:page_part, 
+                                      :name => "content", 
+                                      :content => "define editable text method **strong**", 
+                                      :filter_name => "markdown") 
+
+
+    @request = mock_model("Request", :host => "test.com", :protocol => 'http')
+    @controller.instance_variable_set(:@page, @page)
+    @controller.instance_variable_set(:@request, @request)
+    @controller.instance_variable_set(:@current_site, site)
+    @cmsp = MoustacheCms::Mustache::CmsPage.new(@controller)
+  end
+end
