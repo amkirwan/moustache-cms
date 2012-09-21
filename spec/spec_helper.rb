b8e2811b51ea6e693e0bbc6d2d4f6889e493b6f3
Anthony Kirwan <amkirwan@gmail.com>
2012-09-20 23:11:39 -0400

    moved spec_helpers to support dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ Spork.prefork do
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories. 
-  Dir[Rails.root.join("spec/spec_helpers/*.rb")].each {|f| require f}  
+  Dir[Rails.root.join("spec/support/*.rb")].each {|f| require f}  
 
   RSpec.configure do |config|
     # == Mock Framework
