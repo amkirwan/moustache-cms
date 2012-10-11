9b3cbbafe64c9d1f6db0fc81462290e9ba1388f8
Anthony Kirwan <amkirwan@gmail.com>
2012-10-11 16:23:46 -0500

    removed common validations from article_collection

--- app/models/article_collection.rb
+++ app/models/article_collection.rb
@@ -13,21 +13,6 @@ class ArticleCollection < BaseCollection
   has_many :articles
   has_and_belongs_to_many :editors, :class_name => "User", :inverse_of => :article_collections
 
-  # -- Validations -------
-  validates :name,
-            :presence => true,
-            :uniqueness => { :scope => :site_id }
-
-  validates :site_id,
-            :presence => true
-
-  validates :created_by_id,
-            :presence => true
-
-  validates :updated_by_id,
-            :presence => true
-
-
   # -- Class Methods --
   def self.articles_by_collection_name(name)
     article_collection = self.where(:name => name.to_s).first
