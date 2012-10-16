ab8622bca3a79c0f67016001ba0b4d1bc02b9c26
Anthony Kirwan <amkirwan@gmail.com>
2012-10-16 13:10:53 -0500

    refactored user abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,66 +5,70 @@ class Ability
     user ||= User.new
 
     if user.role? :editor
-      can :read, User, :site_id => user.site_id
-      can [:update, :destroy], User, :username => user.username, :site_id => user.site_id
-      can :change_password, User, :id => user.id, :site_id => user.site_id
-
-      can :read, Page, :site_id => user.site_id
-      can [:update, :destroy], Page do |page|
-        page.editors.include?(user) && page.site_id == user.site_id
-      end
-      
-      can :read, ArticleCollection, :site_id => user.site_id   
-      can :manage, Article do |article|
-        article.article_collection.editors.include?(user) && article.site_id == user.site_id
-      end
-
-      can :read, AssetCollection, :site_id => user.site_id   
-      can :manage, SiteAsset do |site_asset|
-        site_asset._parent.site_id == user.site_id
-      end
-
-      can :manage, PagePart do |page_part|
-        page_part._parent.site_id == user.site_id
-      end
-
-      can :manage, MetaTag do |meta_tag|
-        if meta_tag._parent.respond_to?(:site_id)
-          meta_tag._parent.site_id == user.site_id
-        end
-      end
-      cannot :manage, [Site, Layout, ThemeAsset, Snippet]
+      set_editor_roles(user)
     end
 
     if user.role? :designer
-      can :manage, [Layout, ThemeCollection, Snippet, Author], :site_id => user.site_id  
-      can :manage, ThemeAsset do |theme_asset|
-        theme_asset._parent.site_id == user.site_id
-      end
+      set_designer_roles(user)
     end
 
     if user.role? :admin
-      #can :manage, Site, :id => user.site_id
-      can [:read, :create], Site
-      can [:update, :destroy], Site, :id => user.site_id
-      can :manage, Page, :site_id => user.site_id
-      can :manage, ArticleCollection, :site_id => user.site_id
-      can :manage, Article, :site_id => user.site_id
-      can :manage, AssetCollection, :site_id => user.site_id
+      set_admin_roles(user)
+    end
+  end
 
-      can :manage, MetaTag do |meta_tag|
-        if meta_tag._parent.class.name == "Site"
-          meta_tag._parent.id == user.site_id
-        end
-      end
+  private
 
-      can :manage, CustomField
+  def set_editor_roles(user)
+    can :read, [User, Page, ArticleCollection, AssetCollection], :site_id => user.site_id  
 
-      can :manage, User, :site_id => user.site_id
-      cannot :change_password, User do |u|
-        u.id != user.id
-      end
+    [SiteAsset, PagePart, MetaTag].each do |klass| 
+      set_role(klass, user, :manage)
     end
 
+    can [:update, :destroy], User, :username => user.username, :site_id => user.site_id
+    can :change_password, User, :id => user.id, :site_id => user.site_id
+
+    can [:update, :destroy], Page do |page|
+      page.editors.include?(user) && page.site_id == user.site_id
+    end
+    
+    can :manage, Article do |article|
+      article.article_collection.editors.include?(user) && article.site_id == user.site_id
+    end
+
+    cannot :manage, [Site, Layout, ThemeAsset, Snippet]    
   end
+
+  def set_designer_roles(user)
+    can :manage, [Layout, ThemeCollection, Snippet, Author], :site_id => user.site_id  
+    can :manage, ThemeAsset do |theme_asset|
+      theme_asset._parent.site_id == user.site_id
+    end  
+  end
+
+  def set_admin_roles(user)
+    can [:read, :create], Site
+    can [:update, :destroy], Site, :id => user.site_id
+    can :manage, [Page, ArticleCollection, Article, AssetCollection, User], :site_id => user.site_id
+    cannot :change_password, User do |u|
+      u.id != user.id
+    end
+
+    can :manage, MetaTag do |meta_tag|
+      if meta_tag._parent.class.name == "Site"
+        meta_tag._parent.id == user.site_id
+      end
+    end
+    can :manage, CustomField
+  end
+
+  def set_role(klass, user, *roles)
+    can roles, klass do |obj|
+      if obj._parent.respond_to?(:site_id)
+        obj._parent.site_id == user.site_id
+      end
+    end
+  end
+
 end
