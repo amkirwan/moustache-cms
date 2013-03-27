f262ec1d3145b46e1441876acdac29fc7f7942f2
Anthony Kirwan <amkirwan@gmail.com>
2013-03-27 14:53:09 -0500

    added option to render image tag without fingerprint

--- lib/moustache_cms/mustache/media_tags.rb
+++ lib/moustache_cms/mustache/media_tags.rb
@@ -6,9 +6,16 @@ module MoustacheCms
         lambda do |text|
           hash = parse_text(text)
           image = @current_site.site_asset_by_name(hash['collection_name'], hash['name'])
+
+          if hash['fingerprint'] == 'false'
+            src = image.asset.url
+          else
+            src = image.url_md5 
+          end
+
           unless image.nil?
             engine = gen_haml('image.haml')
-            engine.render(nil, {:src => image.url_md5, :id => hash['id'], :class_name => hash['class'], :alt => hash['alt'], :title => hash['title']})
+            engine.render(nil, {:src => src, :id => hash['id'], :class_name => hash['class'], :alt => hash['alt'], :title => hash['title']})
           end
         end
       end
--- spec/lib/moustache_cms/mustache/media_tags.rb
+++ spec/lib/moustache_cms/mustache/media_tags.rb
@@ -12,6 +12,14 @@ describe MoustacheCms::Mustache::MediaTags do
     @asset_collection.site_assets << @image
     @asset_collection.save
 
+    @theme_collection = FactoryGirl.create(:theme_collection, :name => 'foobar', :site => site)
+    @theme_collection.theme_assets << @theme_asset_image = FactoryGirl.build(:theme_asset, :name => "image", :asset => AssetFixtureHelper.open("rails.png"), :content_type => "image/png", :creator_id => user.id, :updator_id => user.id)
+    @theme_collection.theme_assets << @theme_asset_css = FactoryGirl.build(:theme_asset, :name => "css",  :asset => AssetFixtureHelper.open("theme_css.css"), :content_type => "text/css", :creator_id => user.id, :updator_id => user.id)
+    @theme_collection.theme_assets << @theme_asset_js = FactoryGirl.build(:theme_asset, :name => "js", :asset => AssetFixtureHelper.open("theme_js.js"), :creator_id => user.id, :updator_id => user.id, :content_type => 'text/javascript')
+    @theme_collection.theme_assets << @theme_asset_type = FactoryGirl.build(:theme_asset, :name => "other", :asset => AssetFixtureHelper.open("Inconsolata.otf"), :content_type => "application/octet-stream", :creator_id => user.id, :updator_id => user.id)
+    @theme_collection.save
+
+
     @page.page_parts << FactoryGirl.build(:page_part, 
                                       :name => "main_content", 
                                       :content => "{{#image}}collection_name:blog, name:rails{{/image}}",
@@ -22,6 +30,10 @@ describe MoustacheCms::Mustache::MediaTags do
                                       :content => "{{#image}}collection_name:blog, name:rails, alt:rails logo{{/image}}",
                                       :filter_name => "markdown") 
 
+    @page.page_parts << FactoryGirl.build(:page_part, 
+                                      :name => "foobar_image", 
+                                      :content => "{{#image}}theme_collection_name:foobar, name:image, alt:theme image{{/image}}",
+                                      :filter_name => "markdown") 
     @page.save
     @cmsp.instance_variable_set("@page", @page)
   end
@@ -44,15 +56,20 @@ describe MoustacheCms::Mustache::MediaTags do
       @cmsp.page_part_main_content.should == %{<p><img alt="rails logo" src="#{@image.url_md5}" /></p>}
     end
 
-    it "should render the image tag with an alt acnd class" do
+    it "should render the image tag with an alt and class" do
       set_page_part_content 'main_content' do
         "{{#image}}collection_name:blog, name:rails, alt:rails logo, class:rails-image{{/image}}"
       end
       @cmsp.page_part_main_content.should == %{<p><img alt="rails logo" class="rails-image" src="#{@image.url_md5}" /></p>}
     end
+
+    it "should render the the image src without the fingerprint" do
+      set_page_part_content 'main_content' do
+        "{{#image}}collection_name:blog, name:rails, alt:rails logo, class:rails-image, fingerprint:false{{/image}}"
+      end
+      @cmsp.page_part_main_content.should == %{<p><img alt="rails logo" class="rails-image" src="#{@image.asset.url}" /></p>}
+    end
+
   end
 
 end
-
-  
-
