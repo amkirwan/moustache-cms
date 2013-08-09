4d403a3f166095ee898e19c69a11c3e1bc0f2002
Anthony Kirwan <amkirwan@gmail.com>
2013-08-09 11:07:46 -0500

    add dynamic methods

--- lib/moustache_cms/time_formatted.rb
+++ lib/moustache_cms/time_formatted.rb
@@ -6,29 +6,48 @@ module MoustacheCms
       @published_at = klass.current_state.time
       @created_at = klass.created_at
       @updated_at = klass.updated_at
+
+      @methods =  %w(formatted_date formatted_time formatted_time_zone formatted_date_and_time formatted_date_and_time_with_zone formatted_date_and_time datetime_date datetime_iso8601 full_weekday_name abbv_weekday_name full_month_name abbv_month_name year month day zone)
     end  
 
-    def formatted_date_and_time_with_zone(time=@published_at)
-      return "" if time.nil?
-      "#{formatted_date(time)} at #{formatted_time(time)} #{formatted_time_zone(time)}".strip
-    end
 
-    # def respond_to?(method)
-    #   method_name = method.to_s
-    #   if method_name =~ /^(published_at)_(.*)/ 
-    #     return true
-    #   else
-    #     super
-    #   end
-    # end
+    def respond_to?(method)
+      method_name = method.to_s
+      @methods.each do |m|
+        pub_method = 'published_at_' + m
+        created_at_method = 'created_at_' + m
+        updated_at_method = 'updated_at_' + m
+        case method_name
+        when m, pub_method, created_at_method, updated_at_method
+          return true
+        end
+      end
+      super
+    end
     
     def method_missing(method, *args, &block)
       method_name = method.to_s
       if method_name =~ /^(published_at)_(.*)/ && self.respond_to?($2)
           self.send($2, self.instance_variable_get('@' + $1))
+      elsif method_
       else
         super
       end
+    end
+
+    def formatted_date(time)
+      return "" if time.nil?
+      time.strftime("%e %b %Y").strip
+    end
+    
+    def formatted_time(time)
+      return "" if time.nil?
+      time.strftime("%l%P").strip
+    end
+    
+    def formatted_time_zone(time)
+      return "" if time.nil?
+      time.strftime("%Z").strip
     end
 
     def formatted_date_and_time
@@ -36,19 +55,9 @@ module MoustacheCms
       "#{formatted_date} at #{formatted_time}".strip
     end
     
-    def formatted_date(time)
+    def formatted_date_and_time_with_zone(time=@published_at)
       return "" if time.nil?
-      time.strftime("%e %b %Y").strip
-    end
-
-    def formatted_time(time)
-      return "" if time.nil?
-      time.strftime("%l%P").strip
-    end
-
-    def formatted_time_zone(time)
-      return "" if time.nil?
-      time.strftime("%Z").strip
+      "#{formatted_date(time)} at #{formatted_time(time)} #{formatted_time_zone(time)}".strip
     end
 
     def datetime_date
--- spec/lib/moustache_cms/time_formatted_spec.rb
+++ spec/lib/moustache_cms/time_formatted_spec.rb
@@ -16,6 +16,18 @@ describe MoustacheCms::TimeFormatted do
     @dummy_class = MoustacheCms::TimeFormatted.new(@dummy_time_class)
   end
 
+  describe "respond_to?" do
+    it "should respond to the following methods" do
+      methods =  %w(formatted_date formatted_time formatted_time_zone formatted_date_and_time formatted_date_and_time_with_zone formatted_date_and_time datetime_date datetime_iso8601 full_weekday_name abbv_weekday_name full_month_name abbv_month_name year month day zone)
+      methods.each do |m|
+        @dummy_class.should respond_to m
+        @dummy_class.should respond_to 'published_at_' + m
+        @dummy_class.should respond_to 'created_at_' + m
+        @dummy_class.should respond_to 'updated_at_' + m
+      end
+    end
+  end
+
   describe "#status_formatted_date_time" do
     it "returns the abbreviated time zone" do
       @dummy_class.formatted_time_zone.should == @current_state.time.strftime("%Z").strip  
@@ -30,7 +42,7 @@ describe MoustacheCms::TimeFormatted do
     end
 
     it "returns a humanized formatted date time of the current state with the time zone" do
-      @dummy_class.published_at_formatted_date_and_time_with_zone.should == @current_state.time.strftime("%e %b %Y at %l%P %Z").squeeze(" ").strip  
+      @dummy_class.formatted_date_and_time_with_zone.should == @current_state.time.strftime("%e %b %Y at %l%P %Z").squeeze(" ").strip  
     end
 
     it "returns a humanized formatted date time of the current state" do
