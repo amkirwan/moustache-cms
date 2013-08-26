96054595b231932ef5fdd46c78763b82c714dd92
Anthony Kirwan <amkirwan@gmail.com>
2013-08-26 14:05:16 -0500

    fixed custom page full path search

--- app/controllers/cms_site_controller.rb
+++ app/controllers/cms_site_controller.rb
@@ -52,7 +52,7 @@ class CmsSiteController < ApplicationController
   end
   
   def render_404
-    if @page = @current_site.page_by_full_path("404")
+    if @page = @current_site.page_by_full_path("/404")
       render :text => MoustacheCms::Mustache::CmsPage.new(self).render, :status => 404
     else 
       render :file => "#{Rails.root}/public/404.html", :status => 404
