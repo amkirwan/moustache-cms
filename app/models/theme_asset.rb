4d0956443a7f38535208a46b3f24c7fc47908880
Anthony Kirwan <amkirwan@gmail.com>
2014-03-17 11:35:56 -0500

    use asset pipeline for theme_assets

--- app/models/theme_asset.rb
+++ app/models/theme_asset.rb
@@ -1,52 +1,33 @@
-class ThemeAsset < MoustacheAsset 
+class ThemeAsset 
+  include Mongoid::Document
 
-  attr_accessible :theme_asset_attributes,
-                  :custom_fields_attributes
+  attr_accessible :asset_path, :content_type
 
   # -- Fields ---
-  field :description
-  mount_uploader :asset, ThemeAssetUploader
+  field :asset_path 
+  field :content_type
+
+  after_initialize do |theme_asset|
+    theme_asset.asset
+    theme_asset.content_type = theme_asset.asset.content_type
+  end
+
+  def asset
+    @asset ||= MoustacheCms::Application.assets.find_asset(self.asset_path)
+  end
+
+  def content_type
+    asset.content_type
+  end
 
   # -- Associations ----------
-  embedded_in :article_collection  
-  embeds_many :custom_fields, :as => :custom_fieldable
-  accepts_nested_attributes_for :custom_fields
-
-  # -- Callbacks
-  before_save :update_asset_attributes
-  before_update :recreate
-
-  set_asset_folder :theme_assets
+  belongs_to :theme_collection
 
   # -- Class Methods --------
-  scope :css_files, ->{ where(:content_type => "text/css") }
-  scope :js_files, ->{ where(:content_type => /.*javascript$/i) }
-  scope :javascripts, ->{ where(:content_type => /.*javascript$/i) }
-  scope :images, ->{ where(:content_type => /^image\/*/i) }
-  scope :other_files, ->{ where({ :content_type => {"$nin" => ['text/css', 'application/javascript', 'application/x-javascript', 'text/javascript', 'image/jpg', 'image/jpeg', 'image/gif', 'image/png', 'image/vnd.microsoft.icon'] } }) } 
-
-  scope :find_by_name, ->(name) { where(:name => name) }
-
-  def self.find_by_content_type_and_site_id(opts={})
-    [:content_type, :site].inject(scoped) do |combined_scope, attr| 
-      combined_scope.where(attr => opts[attr])
-    end
-  end
-
-  def css_file_by_name(name)
-    self.css_files.where(:name => name).first
-  end
-  
-  def update_file_content(file_contents)
-    if self.content_type == "text/css" || self.content_type =~ /javascript/ 
-      %W(#{self.asset.path} #{self.current_path_md5}).each do |file_path|
-        File.open(file_path, "w") { |f| f.write(file_contents) }
-      end
-    elsif !File.exists?(self.current_path_md5)
-      File.rename(File.join(Rails.root, 'public', self.store_dir_md5, self.filename_md5_was), self.current_path_md5)
-    else 
-     true
-    end 
-  end
+  scope :css_files, ->{ where(content_type: "text/css") }
+  scope :js_files, ->{ where(:content_type: /.*javascript$/i) }
+  scope :javascripts, ->{ where(:content_type: /.*javascript$/i) }
+  scope :images, ->{ where(content_type: /^image\/*/i) }
+  scope :other_files, ->{ where({ content_type: {"$nin" => ['text/css', 'application/javascript', 'application/x-javascript', 'text/javascript', 'image/jpg', 'image/jpeg', 'image/gif', 'image/png', 'image/vnd.microsoft.icon'] } }) } 
 
 end
--- app/models/theme_collection.rb
+++ app/models/theme_collection.rb
@@ -1,14 +1,31 @@
-class ThemeCollection < MoustacheCollection::Metal
+class ThemeCollection 
+  include Mongoid::Document
+  include Mongoid::Timestamps
 
+  include MoustacheCms::Siteable
+  include MoustacheCms::CreatedUpdatedBy
   # -- Callbacks ---
-  before_destroy :remove_folder 
+  # before_destroy :remove_folder 
   
   created_updated(:theme_collections)
-  collectable(:theme_assets)
   
-  def remove_folder
-    destroy_collection_folder('theme_assets')
+  attr_accessible :name
+
+  # -- Fields ---
+  field :name
+
+  # -- Associations ----------
+  has_many :theme_assets
+
+  # -- Initialize assets ---
+  after_initialize do |theme_collection|
+    if theme_collection.persisted?
+      theme_collection.theme_assets = nil
+      files = Dir.glob("#{Rails.root}/vendor/assets/*/#{theme_collection.name}/**")
+      files.each do |file|
+        theme_collection.theme_assets << ThemeAsset.new(asset_path: file)
+      end
+    end
   end
-
+  
 end
-
